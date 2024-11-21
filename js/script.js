81c2d709375c0b5f80fdf1d0c8f73e786485ec1b
Emanuel N. <86560624+emanuelnn@users.noreply.github.com>
2024-11-20 22:21:56 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ function changeBackgroundColor() {
     const particlesDiv = document.getElementById('particles-js');
     const body = document.body;
 
-    if (currentHour >= 7 && currentHour < 18) {
+    if (currentHour >= 7 && currentHour <= 18) {
         particlesDiv.style.backgroundColor = "#b61924";  // Entre 7h e 18h (alterar cor do background para "dia")
         body.style.backgroundColor = "#b61924";
     } else {
-        particlesDiv.style.backgroundColor = "#080707";  // Entre 18h e 6h (alterar cor do background para "noite")
+        particlesDiv.style.backgroundColor = "#080707";  // Entre 18h e 7h (alterar cor do background para "noite")
         body.style.backgroundColor = "#080707";
     }
 }
